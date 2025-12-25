c22783c799c230af3e15d257f8d63b23e92bbaf6
Yasitha NV <mryasithanv@gmail.com>
2025-12-25 17:22:06 +0530

    impliment Nav Bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,16 @@ import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import BuyCredit from './pages/BuyCredit'
 import Result from './pages/Result'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
+
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28
+        min-h-screen bg-gradient-to-b from-teal-100
+       to-orange-100'>
+
+      <Navbar/>
 
       <Routes>
 
