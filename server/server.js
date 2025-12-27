0bd1391ba41a90ef9d0ec652749a0c2b33be1e59
Yasitha NV <mryasithanv@gmail.com>
2025-12-27 18:56:31 +0530

    implement User Reg&Login

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 
 const PORT = process.env.PORT || 4000
@@ -11,6 +12,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 await connectDB()
+
+
+app.use('/api/user',userRouter)
 
 app.get('/' , (req ,res)=> res.send("API WORKING "))
 
